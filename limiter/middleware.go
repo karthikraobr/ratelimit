7a758825a0e4dfa597c206b8460ad5ba0bcfaf5d
kaarthik.rao@goflink.com <karthik.0703@gmail.com>
2021-09-24 09:45:01 +0200

    fix variable

--- limiter/middleware.go
+++ limiter/middleware.go
@@ -9,9 +9,9 @@ import (
 func NewRateLimiterMiddleware(limit time.Duration, burst int, next http.HandlerFunc) http.Handler {
 	rateLimitter := NewRateLimiter(limit, burst)
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		email := struct {
+		var email struct {
 			Email string `json:"email"`
-		}{}
+		}
 		if err := json.NewDecoder(r.Body).Decode(&email); err != nil {
 			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 			return
